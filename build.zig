3295a8595d25276d73ad0813ad06416dd8e99dc0
Slava Mostovoy <mif.open@gmail.com>
2024-04-09 23:07:46 +0200

    latest zig

--- build.zig
+++ build.zig
@@ -13,12 +13,12 @@ const Program = struct {
 pub fn link(
     b: *std.Build,
     exe: *std.Build.Step.Compile,
-    target: std.zig.CrossTarget,
+    target: std.Target.Query,
     optimize: std.builtin.Mode,
 ) void {
     const lib = getRaylib(b, target, optimize);
 
-    const target_os = exe.target.toTarget().os.tag;
+    const target_os = exe.rootModuleTarget().os.tag;
     switch (target_os) {
         .windows => {
             exe.linkSystemLibrary("winmm");
@@ -60,10 +60,10 @@ pub fn link(
     exe.linkLibrary(lib);
 }
 
-var _raylib_lib_cache: ?*std.build.Step.Compile = null;
+var _raylib_lib_cache: ?*std.Build.Step.Compile = null;
 pub fn getRaylib(
     b: *std.Build,
-    target: std.zig.CrossTarget,
+    target: std.Target.Query,
     optimize: std.builtin.Mode,
 ) *std.Build.Step.Compile {
     if (_raylib_lib_cache) |lib| return lib else {
@@ -84,30 +84,26 @@ pub fn getModule(b: *std.Build, comptime rl_path: []const u8) *std.Build.Module 
     if (b.modules.contains("raylib")) {
         return b.modules.get("raylib").?;
     }
-    return b.addModule("raylib", .{ .source_file = .{ .path = rl_path ++ "/lib/raylib-zig.zig" } });
+    return b.addModule("raylib", .{ .root_source_file = .{ .path = rl_path ++ "/lib/raylib-zig.zig" } });
 }
 
 fn getModuleInternal(b: *std.Build) *std.Build.Module {
     if (b.modules.contains("raylib")) {
         return b.modules.get("raylib").?;
     }
-    return b.addModule("raylib", .{ .source_file = .{ .path = "lib/raylib-zig.zig" } });
+    return b.addModule("raylib", .{ .root_source_file = .{ .path = "lib/raylib-zig.zig" } });
 }
 
 pub const math = struct {
     pub fn getModule(b: *std.Build, comptime rl_path: []const u8) *std.Build.Module {
-        const raylib = rl.getModule(b, rl_path);
         return b.addModule("raylib-math", .{
-            .source_file = .{ .path = rl_path ++ "/lib/raylib-zig-math.zig" },
-            .dependencies = &.{.{ .name = "raylib-zig", .module = raylib }},
+            .root_source_file = .{ .path = rl_path ++ "/lib/raylib-zig-math.zig" },
         });
     }
 
     fn getModuleInternal(b: *std.Build) *std.Build.Module {
-        const raylib = rl.getModuleInternal(b);
         return b.addModule("raylib-math", .{
-            .source_file = .{ .path = "lib/raylib-zig-math.zig" },
-            .dependencies = &.{.{ .name = "raylib-zig", .module = raylib }},
+            .root_source_file = .{ .path = "lib/raylib-zig-math.zig" },
         });
     }
 };
@@ -185,11 +181,11 @@ pub fn build(b: *std.Build) !void {
     const raylib_math = rl.math.getModuleInternal(b);
 
     for (examples) |ex| {
-        if (target.getOsTag() == .emscripten) {
+        if (target.result.os.tag == .emscripten) {
             const exe_lib = emcc.compileForEmscripten(b, ex.name, ex.path, target, optimize);
-            exe_lib.addModule("raylib", raylib);
-            exe_lib.addModule("raylib-math", raylib_math);
-            const raylib_lib = getRaylib(b, target, optimize);
+            exe_lib.root_module.addImport("raylib", raylib);
+            exe_lib.root_module.addImport("raylib-math", raylib_math);
+            const raylib_lib = getRaylib(b, target.query, optimize);
 
             // Note that raylib itself isn't actually added to the exe_lib
             // output file, so it also needs to be linked with emscripten.
@@ -209,9 +205,9 @@ pub fn build(b: *std.Build) !void {
                 .optimize = optimize,
                 .target = target,
             });
-            rl.link(b, exe, target, optimize);
-            exe.addModule("raylib", raylib);
-            exe.addModule("raylib-math", raylib_math);
+            rl.link(b, exe, target.query, optimize);
+            exe.root_module.addImport("raylib", raylib);
+            exe.root_module.addImport("raylib-math", raylib_math);
             const run_cmd = b.addRunArtifact(exe);
             const run_step = b.step(ex.name, ex.desc);
             run_step.dependOn(&run_cmd.step);
